d27741a6cfb5c69f9b0b7008a1f5f2bb7154b04b
AyushYadav1113 <ayushyadav212121@gmail.com>
2026-04-08 00:14:42 +0530

    Count digit

--- NoOfDigit.py
+++ NoOfDigit.py
@@ -1,11 +1,17 @@
+# n = int(input("Enter the number: "))
+
+# if n == 0:
+#     c = 1
+# else:
+#     c = 0
+#     while n != 0:
+#         n = n // 10
+#         c += 1
+
+# print("The number of digits is:", c)
+
+
+
+# Alternative method using Built in Function
 n = int(input("Enter the number: "))
-
-if n == 0:
-    c = 1
-else:
-    c = 0
-    while n != 0:
-        n = n // 10
-        c += 1
-
-print("The number of digits is:", c)
+print("The number of digits is:", len(n))
