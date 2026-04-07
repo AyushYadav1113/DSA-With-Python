b9c573eb8b453b1d90dc0d4004741ba9388e80ee
AyushYadav1113 <ayushyadav212121@gmail.com>
2026-04-08 00:49:35 +0530

    factor

--- factors.py
+++ factors.py
@@ -1,4 +1,12 @@
+# n = int(input("Enter the number: "))
+# for i in range(1, n + 1):
+#     if n % i == 0:
+#         print(i)
+
 n = int(input("Enter the number: "))
-for i in range(1, n + 1):
-    if n % i == 0:
-        print(i)
+result = []
+for i in range(1, n//2):
+    if n% i==0 :
+        result.append(i)
+result.append(n)
+print(result)
