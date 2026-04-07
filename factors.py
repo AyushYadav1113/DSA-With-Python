6711f396195b2a56cd8345da304904a9304b84fe
AyushYadav1113 <ayushyadav212121@gmail.com>
2026-04-08 01:02:47 +0530

    factor

--- factors.py
+++ factors.py
@@ -1,12 +1,30 @@
-# n = int(input("Enter the number: "))
-# for i in range(1, n + 1):
-#     if n % i == 0:
-#         print(i)
+# # n = int(input("Enter the number: "))
+# # for i in range(1, n + 1):
+# #     if n % i == 0:
+# #         print(i)
 
+
+
+
+
+# n = int(input("Enter the number: "))
+# result = []
+# for i in range(1, n//2):
+#     if n% i==0 :
+#         result.append(i)
+# result.append(n)
+# print(result)
+
+from math import sqrt
 n = int(input("Enter the number: "))
 result = []
-for i in range(1, n//2):
-    if n% i==0 :
+for i in range (1, sqrt(n)+1):
+    if n%i==0:
         result.append(i)
-result.append(n)
+        if n//i != i:
+            result.append(n//i)
+result.sort()
 print(result)
+ 
+
+
